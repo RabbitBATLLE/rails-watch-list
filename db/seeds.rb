672daae4584954b669b55b455cf6d3a43abd48d2
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-19 16:12:59 +0200

    Youpi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+6.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Book.genre,
+    poster_url: Faker::Internet.url,
+    rating: Faker::Number.positive
+  )
+  movie.save
+end
